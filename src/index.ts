c502cd93df053c175821557df4291d79b8cd6584
stephenmcguckin <stephen.mcguckin@dat.com>
2020-11-20 15:19:47 -0800

    removes express

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import * as express from 'express';
-import {json} from 'body-parser';
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(json());
 
-app.get('/', (req, res) => {
-    res.status(200).send();
-});
-
-app.post('/', (req, res) => {
-    res.status(200).send(req.body);
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
