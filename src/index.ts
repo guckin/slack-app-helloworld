421851125244e5f3aa7f437b3ed72257f5a05431
stephenmcguckin <stephen.mcguckin@dat.com>
2020-11-20 15:35:25 -0800

    slack bot hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import {createEventAdapter} from '@slack/events-api';
 
+const port = Number(process.env.PORT) || 3000;
+const slackSigningSecret = 'c4de58a11177e994f9467790ecabad2f';
+const slackEvents = createEventAdapter(slackSigningSecret);
+
+slackEvents.on('message', (event) => {
+    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+(async () => {
+    await slackEvents.start(port);
+    console.log(`Listening`);
+})();
