2893be564827f4b6b496ff2697d3acd13a9e9358
stephenmcguckin <stephen.mcguckin@dat.com>
2020-11-20 15:15:15 -0800

    removing this code just need it to get bootstrapped

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
+import {json} from 'body-parser';
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(json());
 
 app.get('/', (req, res) => {
     res.status(200).send();
